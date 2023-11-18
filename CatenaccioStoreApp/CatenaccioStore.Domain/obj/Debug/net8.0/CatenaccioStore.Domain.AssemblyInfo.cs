798322136777d4fc5f0937996df1cd98efe44b19
gchikovanii <gchikovanii25@gmail.com>
2023-11-18 14:48:51 +0400

    Blazor front-app implemented

--- CatenaccioStoreApp/CatenaccioStore.Domain/obj/Debug/net8.0/CatenaccioStore.Domain.AssemblyInfo.cs
+++ CatenaccioStoreApp/CatenaccioStore.Domain/obj/Debug/net8.0/CatenaccioStore.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatenaccioStore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986853b7a0757a682c359cd32ba3fe22477ba619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatenaccioStore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatenaccioStore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
