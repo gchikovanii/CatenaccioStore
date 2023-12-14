a90ec610d4e5cac3af6198e4c7af081ec66feb39
gchikovanii <gchikovanii25@gmail.com>
2023-12-14 14:26:40 +0400

    Angular Material Implemented

--- CatenaccioStoreApp/CatenaccioStore/src/app/app.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule,MatSelectModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
