c7020cb1bcadd805988a4b9ee8d6e4b48454caa5
gchikovanii <gchikovanii25@gmail.com>
2023-12-15 16:33:54 +0400

    Offers Added

--- CatenaccioStoreApp/CatenaccioStore/src/app/Offers/offers.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/Offers/offers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
