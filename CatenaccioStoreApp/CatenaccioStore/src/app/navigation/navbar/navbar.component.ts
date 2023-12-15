2eaa77a55be9d7727ec293c00ad35a0a1aac13f7
gchikovanii <gchikovanii25@gmail.com>
2023-12-15 11:30:46 +0400

    Navbar Responsivness added

--- CatenaccioStoreApp/CatenaccioStore/src/app/navigation/navbar/navbar.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/navigation/navbar/navbar.component.ts
@@ -1,24 +1,38 @@
-import {Component} from '@angular/core';
+import {Component,HostListener } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule,MatButtonModule,MatToolbarModule,MatSidenavModule,RouterOutlet,CommonModule,RouterLink],
+  imports: [FlexLayoutModule,MatIconModule,MatButtonModule,MatToolbarModule,MatSidenavModule,RouterOutlet,CommonModule,RouterLink,MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
 export class NavbarComponent {
-  toggleSidenav() {
-    
+  isSmallScreen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.checkScreenSize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isSmallScreen = this.breakpointObserver.isMatched('(max-width: 900px)');
+  }
   openRegistration() {
     // Implement logic to open the registration form
   }
