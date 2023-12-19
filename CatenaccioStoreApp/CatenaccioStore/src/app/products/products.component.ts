236639cc896ba819e1845f3481a1496d7be0c548
gchikovanii <gchikovanii25@gmail.com>
2023-12-19 14:27:58 +0400

    search bar added

--- CatenaccioStoreApp/CatenaccioStore/src/app/products/products.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/products/products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,MatFormFieldModule,MatPaginatorModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
