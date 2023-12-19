643dc21bbc35fd2ab8f0554901e95e531908413e
gchikovanii <gchikovanii25@gmail.com>
2023-12-19 11:02:10 +0400

    Basket Added

--- CatenaccioStoreApp/CatenaccioStore/src/app/basket/basket.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/basket/basket.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-basket',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.css'
 })
 export class BasketComponent {
+  quantity: number = 0;
 
+  increment() {
+    this.quantity++;
+  }
+
+  decrement() {
+    if (this.quantity > 0) {
+      this.quantity--;
+    }
+  }
 }
