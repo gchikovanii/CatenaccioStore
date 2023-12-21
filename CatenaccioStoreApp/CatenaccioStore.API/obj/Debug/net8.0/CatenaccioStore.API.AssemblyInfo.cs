4b3e3004b630b1bad6d97c0009c3031cfa5235ed
gchikovanii <gchikovanii25@gmail.com>
2023-12-21 10:57:45 +0400

    Rewriting to no standalone project

--- CatenaccioStoreApp/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
+++ CatenaccioStoreApp/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86040d88a3ec8bb8c1c51d7ab48e8dd7e1d74d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
