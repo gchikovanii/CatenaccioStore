c45886d6b1db9d5f7908b991acce62c3c5a17c8a
gchikovanii <gchikovanii25@gmail.com>
2023-12-22 16:24:14 +0400

    registration changed

--- CatenaccioStoreApp/CatenaccioStore/src/app/account/account.component.ts
+++ CatenaccioStoreApp/CatenaccioStore/src/app/account/account.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Register } from '../models/register';
 import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar'; 
+
 
 @Component({
   selector: 'app-account',
@@ -10,10 +13,31 @@ import { AuthenticationService } from '../authentication.service';
 export class AccountComponent {
   registerDto = new Register();
 
-  constructor(private _autheticationService: AuthenticationService){}
+  constructor(private _autheticationService: AuthenticationService, private router: Router,private snackBar: MatSnackBar){}
 
-  register(registerDto: Register){
-    this._autheticationService.register(registerDto).subscribe();
+  register(registerDto: Register) {
+    this._autheticationService.register(registerDto).subscribe(
+      () => {
+        // Registration successful
+        this.openSnackBar('Registration successful!', 'Success');
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        // Handle registration error
+        if ((error.status === 500  && error.error === 'already_exists') || error.status == 400) {
+          this.openSnackBar('User with the same credentials already exists.', 'Error');
+        } else {
+          this.openSnackBar('Registration failed. Please try again later.', 'Error');
+        }
+      }
+    );
+  }
+
+  // Helper method to open a snackbar
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 3000,  // Set the duration for the snackbar
+    });
   }
  
 }
