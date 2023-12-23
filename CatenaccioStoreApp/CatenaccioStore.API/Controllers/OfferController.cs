14a5edc70497293744bc95abe21db414a2ef8414
gchikovanii <gchikovanii25@gmail.com>
2023-12-23 07:50:09 +0400

    Offers Controller Added

---
+++ CatenaccioStoreApp/CatenaccioStore.API/Controllers/OfferController.cs
@@ -0,0 +1,64 @@
+﻿using CatenaccioStore.API.Infrastructure.constans;
+using CatenaccioStore.Application.Services.Products.Abstraction;
+using CatenaccioStore.Application.Services.Products.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace CatenaccioStore.API.Controllers
+{
+    public class OfferController : BaseController
+    {
+        private readonly IOfferService _offerService;
+
+        public OfferController(IOfferService offerService)
+        {
+            _offerService = offerService;
+        }
+        [Authorize(Roles = UserType.AdminModerator)]
+        [HttpGet]
+        public async Task<IActionResult> GetCategories(CancellationToken token)
+        {
+            try
+            {
+                return Ok(await _offerService.GetAllOffers(token));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+        [Authorize(Roles = UserType.AdminModerator)]
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CancellationToken token, OfferDto input)
+        {
+            try
+            {
+                return Ok(await _offerService.CreateAsync(token, input));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+       
+        [Authorize(Roles = UserType.AdminModerator)]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(CancellationToken token, string categoryName)
+        {
+            try
+            {
+                return Ok(await _offerService.DeleteAsync(token, categoryName));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+
+    }
+}
--- CatenaccioStoreApp/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
+++ CatenaccioStoreApp/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae525030de32de7235ac74e57b2ec343564a46be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
