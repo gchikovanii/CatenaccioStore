ae525030de32de7235ac74e57b2ec343564a46be
gchikovanii <gchikovanii25@gmail.com>
2023-12-23 07:19:48 +0400

    Enntity Finished

--- CatenaccioStoreApp/CatenaccioStore.Domain/Entities/Products/Offer.cs
+++ CatenaccioStoreApp/CatenaccioStore.Domain/Entities/Products/Offer.cs
@@ -3,5 +3,7 @@
     public class Offer : BaseEntity
     {
         public decimal NewPrice { get; set; }
+        public int? ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
